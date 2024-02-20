c57c205c558146a47859e040961c4f5fd845a279
Aagam Golechha <aagamaj1212@gmail.com>
2024-02-20 16:20:54 +0530

    Added Backend Support for Contact Page, using MoongooseJS and MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/contactDance', {useNewUrlParser: true});
 const port = 80;
+app.use(express.urlencoded({extended: true}));
+
+// Defining Mongoose Schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+    age: String,
+    concern: String,
+    address: String
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+module.exports = Contact;
 
 // EXPRESS SPECIFIC CONFIGURATIONS
 app.use('/static', express.static('static'));   // for serving static files
@@ -24,6 +40,15 @@ app.get('/home', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.status(200).render('contact.pug');
+})
+
+app.post('/contact', (req, res) => {
+    const myData = new Contact(req.body);
+    myData.save().then(() => {
+        res.send("Your Concern Has Been Successfully Submitted");
+    }).catch(() => {
+        res.status(404).send("Error Encountred");
+    });
 })
 
 
